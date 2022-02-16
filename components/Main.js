d13a51e86f6aabd0134aaebff1917014e19da294
Nguyễn Hiếu <59680580+hieuls369@users.noreply.github.com>
2022-02-16 16:36:31 +0700

    update UI

--- components/Main.js
+++ components/Main.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
 import Portfolio from './Portfolio'
+import Promos from './Promos'
 
-const Main = () => {
+const Main = ({ thirdWebTokens, sanityTokens, walletAddress }) => {
   return (
     <Wrapper>
-        <Portfolio />
+        <Portfolio walletAddress={walletAddress} sanityTokens={sanityTokens} thirdWebTokens={thirdWebTokens}/>
+        <Promos />
     </Wrapper>
   )
 }
@@ -16,6 +18,10 @@ const Wrapper = styled.div`
     display: flex;
     max-height: calc(100vh - 64px);
     overflow: hidden;
+    overflow-y: scroll;
+    ::-webkit-scrollbar{
+      display: none;
+    }
 
     & div {
         border-radius: 0.4rem;
